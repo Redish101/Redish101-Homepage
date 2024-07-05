60af512f31dc9460888f95b933a72185a04b60f4
Redish101 <i@redish101.top>
2024-07-05 15:22:41 +0800

    chore: 无障碍

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="flex justify-center text-slate-400 font-normal py-8">
+        <footer className="flex justify-center text-slate-800 font-normal py-8">
             Powered by<span className="font-bold">&nbsp;Next.js</span>
         </footer>
     )
