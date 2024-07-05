a738190891c95e748e267c6dabd6cbb8ca4c6147
Redish101 <i@redish101.top>
2024-07-05 09:57:08 +0800

    feat: 导航栏链接

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,6 +2,10 @@ export default function NavBar() {
   return (
     <nav className="bg-nav-background backdrop-blur shadow h-12 flex justify-between items-center px-16 fixed w-full z-100">
       <span className="font-medium">Redish101</span>
+      <div className="text-sm text-gray-800 flex space-x-4">
+        <a href="https://blog.redish101.top">博客</a>
+        <a href="https://github.com/redish101">GitHub</a>
+      </div>
     </nav>
   );
 }
