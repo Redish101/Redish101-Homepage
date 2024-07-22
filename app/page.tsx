1ccd98b857264f842e90297651ed1ceacf8c1470
Redish101 <i@redish101.top>
2024-07-22 14:58:23 +0800

    feat: 活动监测

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "./components/Button";
 
 function Reblog() {
   return (
-    <div className="w-screen h-screen bg-[#0D2F4D] text-background p-12 flex flex-col justify-center items-center gap-16 overflow-hidden">
+    <div className="bg-[#0D2F4D] text-background p-12 flex flex-col justify-center items-center gap-16 overflow-hidden">
       <div className="flex flex-col gap-4 items-center mt-48">
         <h2 className="font-mono font-bold text-6xl text-white">reblog</h2>
         <span className="text-[#ffffff90] text0">
@@ -49,6 +49,56 @@ function QrcodeGenerator() {
   );
 }
 
+async function ReMonitor() {
+  const monitorRes = await fetch("https://redish101-remonitor.deno.dev");
+  const monitorData = await monitorRes.json();
+
+  const lastestGithubEventTimeDiff =
+    monitorData["data"]["lastestGithubEventTimeDiff"];
+  const latestReMonitorEventTimeDiff =
+    monitorData["data"]["latestReMonitorEventTimeDiff"];
+
+  const msToStr = (ms: number) => {
+    const sec = Math.floor(ms / 1000);
+    const min = Math.floor(sec / 60);
+    const hour = Math.floor(min / 60);
+    const day = Math.floor(hour / 24);
+
+    const strArr = [];
+    if (day > 0) strArr.push(`${day}天`);
+    if (hour > 0) strArr.push(`${hour % 24}小时`);
+    if (min > 0) strArr.push(`${min % 60}分`);
+    if (sec > 0) strArr.push(`${sec % 60}秒`);
+
+    return strArr.join("");
+  };
+
+  if (!monitorData["success"]) return null;
+
+  return (
+    <div className="p-12 flex flex-col gap-16 h-screen justify-center">
+      <div className="flex flex-col gap-4 ">
+        <h2 className="font-bold text-2xl text-slate-600">remonitor</h2>
+        <h2 className="font-bold text-6xl">活动监测</h2>
+      </div>
+      <div>
+        <div className="font-normal text-2xl">
+          上次GitHub活动于
+          <span className="font-bold font-mono text-teal-700 text-4xl mx-8">
+            {msToStr(lastestGithubEventTimeDiff)}前
+          </span>
+        </div>
+        <div className="font-normal text-2xl">
+          上次remonitor-client正常状态上报于
+          <span className="font-bold font-mono text-teal-700 text-4xl mx-8">
+            {msToStr(latestReMonitorEventTimeDiff)}前
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
@@ -61,6 +111,7 @@ export default function Home() {
         <div className="text-lg text-gray-700">学生，人，活的</div>
       </div>
       <Reblog />
+      <ReMonitor />
       {/* <QrcodeGenerator /> */}
     </div>
   );
